4c8f366b93bfa7a46c7f0c68e60caacc556faed0
stefan.bertos <stefan.bertos@gmail.com>
2023-03-03 09:50:04 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  HeroLayout3 
+ } from './ui-components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeroLayout3 />
     </div>
   );
 }
